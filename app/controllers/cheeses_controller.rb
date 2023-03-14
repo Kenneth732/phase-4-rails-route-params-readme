2f08cb9c11dbca7d74217be4dae80eddce0346e7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-03-14 14:49:16 +0300

    done assign

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,12 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show
+    # find a cheese using the ID from URL
+    cheese = Cheese.find_by(params[:id])
+    # send a JSON reponse using that cheesse object
+    render json: cheese 
+    # byebug
+  end
 
 end
